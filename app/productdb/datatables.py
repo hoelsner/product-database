937301589bff4e4320a6f4ec986d954bda16d2b0
Henry Ölsner <henry@codingnetworker.com>
2016-06-12 14:23:58 +0200

    add additional datatables features

--- app/productdb/datatables.py
+++ app/productdb/datatables.py
@@ -1,3 +1,4 @@
+import re
 from django_datatables_view.base_datatable_view import BaseDatatableView
 from .models import Product
 from django.db.models import Q
@@ -14,6 +15,13 @@ class VendorProductListJson(BaseDatatableView):
         'list_price',
         'tags'
     ]
+    column_order = {
+        "product_id": 0,
+        "description": 1,
+        "list_price": 2,
+        "tags": 3
+    }
+
     vendor_id = 0
 
     def get_initial_queryset(self):
@@ -28,9 +36,58 @@ class VendorProductListJson(BaseDatatableView):
         # use request parameters to filter queryset
         search_string = self.request.GET.get('search[value]', None)
 
+        product_id_get_param = 'columns[' + str(self.column_order["product_id"]) + '][search][value]'
+        description_get_param = 'columns[' + str(self.column_order["description"]) + '][search][value]'
+        list_price_get_param = 'columns[' + str(self.column_order["list_price"]) + '][search][value]'
+        tags_get_param = 'columns[' + str(self.column_order["tags"]) + '][search][value]'
+
+        product_id_search = self.request.GET.get(product_id_get_param, None)
+        description_search = self.request.GET.get(description_get_param, None)
+        list_price_search = self.request.GET.get(list_price_get_param, None)
+        tags_search = self.request.GET.get(tags_get_param, None)
+
         if search_string:
-            qs = qs.filter(Q(product_id__contains=search_string) |
-                           Q(description__contains=search_string))
+            # by default, the search field searched in the Product ID and the description field
+            try:
+                re.compile(search_string)
+                qs = qs.filter(Q(product_id__regex=search_string) |
+                               Q(description__regex=search_string))
+
+            except:
+                qs = qs.filter(Q(product_id__contains=search_string) |
+                               Q(description__contains=search_string))
+
+        if product_id_search:
+            try:
+                re.compile(product_id_search)
+                qs = qs.filter(product_id__regex=product_id_search)
+
+            except:
+                qs = qs.filter(product_id__contains=product_id_search)
+
+        if description_search:
+            try:
+                re.compile(description_search)
+                qs = qs.filter(description__regex=description_search)
+
+            except:
+                qs = qs.filter(description__contains=description_search)
+
+        if list_price_search:
+            try:
+                re.compile(list_price_search)
+                qs = qs.filter(list_price__regex=list_price_search)
+
+            except:
+                qs = qs.filter(list_price__contains=list_price_search)
+
+        if tags_search:
+            try:
+                re.compile(tags_search)
+                qs = qs.filter(tags__regex=tags_search)
+
+            except:
+                qs = qs.filter(tags__contains=tags_search)
 
         return qs
 
@@ -74,6 +131,13 @@ class ListProductsJson(BaseDatatableView):
         'list_price',
         'tags'
     ]
+    column_order = {
+        "vendor": 0,
+        "product_id": 1,
+        "description": 2,
+        "list_price": 3,
+        "tags": 4
+    }
 
     def get_initial_queryset(self):
         # return queryset used as base for futher sorting/filtering
@@ -84,9 +148,68 @@ class ListProductsJson(BaseDatatableView):
         # use request parameters to filter queryset
         search_string = self.request.GET.get('search[value]', None)
 
+        vendor_get_param = 'columns[' + str(self.column_order["vendor"]) + '][search][value]'
+        product_id_get_param = 'columns[' + str(self.column_order["product_id"]) + '][search][value]'
+        description_get_param = 'columns[' + str(self.column_order["description"]) + '][search][value]'
+        list_price_get_param = 'columns[' + str(self.column_order["list_price"]) + '][search][value]'
+        tags_get_param = 'columns[' + str(self.column_order["tags"]) + '][search][value]'
+
+        vendor_search = self.request.GET.get(vendor_get_param, None)
+        product_id_search = self.request.GET.get(product_id_get_param, None)
+        description_search = self.request.GET.get(description_get_param, None)
+        list_price_search = self.request.GET.get(list_price_get_param, None)
+        tags_search = self.request.GET.get(tags_get_param, None)
+
         if search_string:
-            qs = qs.filter(Q(product_id__contains=search_string) |
-                           Q(description__contains=search_string))
+            # by default, the search field searched in the Product ID and the description field
+            try:
+                re.compile(search_string)
+                qs = qs.filter(Q(product_id__regex=search_string) |
+                               Q(description__regex=search_string))
+
+            except:
+                qs = qs.filter(Q(product_id__contains=search_string) |
+                               Q(description__contains=search_string))
+
+        if vendor_search:
+            try:
+                re.compile(vendor_search)
+                qs = qs.filter(vendor__name__regex=vendor_search)
+
+            except:
+                qs = qs.filter(vendor__name__contains=vendor_search)
+
+        if product_id_search:
+            try:
+                re.compile(product_id_search)
+                qs = qs.filter(product_id__regex=product_id_search)
+
+            except:
+                qs = qs.filter(product_id__contains=product_id_search)
+
+        if description_search:
+            try:
+                re.compile(description_search)
+                qs = qs.filter(description__regex=description_search)
+
+            except:
+                qs = qs.filter(description__contains=description_search)
+
+        if list_price_search:
+            try:
+                re.compile(list_price_search)
+                qs = qs.filter(list_price__regex=list_price_search)
+
+            except:
+                qs = qs.filter(list_price__contains=list_price_search)
+
+        if tags_search:
+            try:
+                re.compile(tags_search)
+                qs = qs.filter(tags__regex=tags_search)
+
+            except:
+                qs = qs.filter(tags__contains=tags_search)
 
         return qs
 
--- tests/functional/user_interface/test_views.py
+++ tests/functional/user_interface/test_views.py
@@ -95,7 +95,7 @@ class BrowseProductsByVendor(DestructiveProductDbFunctionalTest):
 
         # the table shows 10 entries from the list (below the table, there is a string "Showing 1 to 10 of \d+ entries"
         dt_wrapper = self.browser.find_element_by_id("product_table_info")
-        self.assertRegex(dt_wrapper.text, r".*Showing 1 to 10 of \d+ entries.*")
+        self.assertRegex(dt_wrapper.text, r"Showing 1 to \d+ of \d+ entries")
 
         # the page reloads and the table contains now the element "C2960X-STACK" as the first element of the table
         table = self.browser.find_element_by_id('product_table')
@@ -156,7 +156,7 @@ class BrowseProductsByVendor(DestructiveProductDbFunctionalTest):
         time.sleep(3)
 
         # the table performs the search function and a defined amount of rows is displayed
-        expected_table_content = """Product ID Description List Price Lifecycle State"""
+        expected_table_content = """Product ID\nDescription\nList Price Lifecycle State"""
         table_rows = [
             'WS-C2960X-24PD-L Catalyst 2960-X 24 GigE PoE 370W, 2 x 10G SFP+, LAN Base 4595.00 USD',
             'WS-C2960X-24PS-L Catalyst 2960-X 24 GigE PoE 370W, 4 x 1G SFP, LAN Base 3195.00 USD',
@@ -166,3 +166,150 @@ class BrowseProductsByVendor(DestructiveProductDbFunctionalTest):
         self.assertIn(expected_table_content, table.text)
         for r in table_rows:
             self.assertIn(r, table.text)
+        search.clear()
+
+        # search product by column
+        self.browser.find_element_by_id("column_search_Product ID").send_keys("WS-C2960X-24P(D|S)-L")
+        table = self.browser.find_element_by_id('product_table')
+        self.assertIn(expected_table_content, table.text)
+        for r in table_rows:
+            self.assertIn(r, table.text)
+        self.browser.find_element_by_id("column_search_Product ID").clear()
+
+        # search description by column
+        self.browser.find_element_by_id("column_search_Description").send_keys("(1|10)G SFP")
+        table = self.browser.find_element_by_id('product_table')
+        self.assertIn(expected_table_content, table.text)
+        for r in table_rows:
+            self.assertIn(r, table.text)
+        self.browser.find_element_by_id("column_search_Description").clear()
+
+        # search description by column
+        self.browser.find_element_by_id("column_search_List Price").send_keys("3195")
+        table = self.browser.find_element_by_id('product_table')
+        self.assertIn(expected_table_content, table.text)
+        self.assertIn(r[1], table.text)
+        self.browser.find_element_by_id("column_search_List Price").clear()
+
+
+class BrowseAllProducts(DestructiveProductDbFunctionalTest):
+    fixtures = ['default_vendors.yaml']
+
+    def test_browse_all_products_view(self):
+        expected_cisco_row = "Cisco Systems C2960X-STACK Catalyst 2960-X FlexStack Plus Stacking Module 1195.00 USD"
+        expected_juniper_row = "Juniper Networks EX-SFP-1GE-LX SFP 1000Base-LX Gigabit Ethernet Optics, 1310nm for " \
+                               "10km transmission on SMF 1000.00 USD"
+
+        # a user hits the browse product list url
+        self.browser.get(self.server_url + reverse("productdb:all_products"))
+        self.browser.implicitly_wait(5)
+
+        # check that the user sees a table
+        time.sleep(5)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertIn("Showing 1 to", page_text)
+
+        # the table has three buttons: Copy, CSV and a PDF
+        dt_buttons = self.browser.find_element_by_class_name("dt-buttons")
+
+        self.assertEqual("PDF", dt_buttons.find_element_by_link_text("PDF").text)
+        self.assertEqual("Copy", dt_buttons.find_element_by_link_text("Copy").text)
+        self.assertEqual("CSV", dt_buttons.find_element_by_link_text("CSV").text)
+
+        # the table shows 10 entries from the list (below the table, there is a string "Showing 1 to 10 of \d+ entries"
+        dt_wrapper = self.browser.find_element_by_id("product_table_info")
+        self.assertRegex(dt_wrapper.text, r"Showing 1 to \d+ of \d+ entries")
+
+        # the page reloads and the table contains now the element "C2960X-STACK" as the first element of the table
+        table = self.browser.find_element_by_id('product_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(expected_cisco_row,
+                      [row.text for row in rows])
+
+        # the page reloads and the table contains now the element "EX-SFP-1GE-LX" as the first element of the table
+        table = self.browser.find_element_by_id('product_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        match = False
+        for i in range(0, 3):
+            match = (expected_juniper_row,
+                     [row.text for row in rows])
+            if match:
+                break
+            time.sleep(3)
+        if not match:
+            self.fail("Element not found")
+
+    def test_browse_all_products_view_csv_export(self):
+        # a user hits the browse product list url
+        self.browser.get(self.server_url + reverse("productdb:all_products"))
+        self.browser.implicitly_wait(3)
+
+        # the user hits the button CSV
+        dt_buttons = self.browser.find_element_by_class_name("dt-buttons")
+        dt_buttons.find_element_by_link_text("CSV").click()
+
+        # the file should download automatically (firefox is configured this way)
+
+        # verify that the file is a CSV formatted field (with ";" as delimiter)
+        file = os.path.join(self.download_dir, "export products.csv")
+        f = open(file, "r+")
+        self.assertEqual("\ufeffVendor;Product ID;Description;List Price;Lifecycle State\n", f.readline())
+        f.close()
+
+    def test_search_function(self):
+        # a user hits the browse product list url
+        self.browser.get(self.server_url + reverse("productdb:all_products"))
+        self.browser.implicitly_wait(3)
+
+        # he enters a search term in the search box
+        search_term = "WS-C2960X-24P"
+        search_xpath = '//div[@class="col-sm-4"]/div[@id="product_table_filter"]/label/input[@type="search"]'
+        search = self.browser.find_element_by_xpath(search_xpath)
+        search.send_keys(search_term)
+        time.sleep(3)
+
+        # the table performs the search function and a defined amount of rows is displayed
+        expected_table_content = """Vendor\nProduct ID\nDescription\nList Price Lifecycle State"""
+        table_rows = [
+            'WS-C2960X-24PD-L Catalyst 2960-X 24 GigE PoE 370W, 2 x 10G SFP+, LAN Base 4595.00 USD',
+            'WS-C2960X-24PS-L Catalyst 2960-X 24 GigE PoE 370W, 4 x 1G SFP, LAN Base 3195.00 USD',
+        ]
+
+        table = self.browser.find_element_by_id('product_table')
+        self.assertIn(expected_table_content, table.text)
+        for r in table_rows:
+            self.assertIn(r, table.text)
+
+        search.clear()
+
+        # search vendor by column
+        self.browser.find_element_by_id("column_search_Vendor").send_keys("Cisco")
+        table = self.browser.find_element_by_id('product_table')
+        self.assertIn(expected_table_content, table.text)
+        for r in table_rows:
+            self.assertIn(r, table.text)
+        self.browser.find_element_by_id("column_search_Vendor").clear()
+
+        # search product by column
+        self.browser.find_element_by_id("column_search_Product ID").send_keys("WS-C2960X-24P(D|S)-L")
+        table = self.browser.find_element_by_id('product_table')
+        self.assertIn(expected_table_content, table.text)
+        for r in table_rows:
+            self.assertIn(r, table.text)
+        self.browser.find_element_by_id("column_search_Product ID").clear()
+
+        # search description by column
+        self.browser.find_element_by_id("column_search_Description").send_keys("(1|10)G SFP")
+        table = self.browser.find_element_by_id('product_table')
+        self.assertIn(expected_table_content, table.text)
+        for r in table_rows:
+            self.assertIn(r, table.text)
+        self.browser.find_element_by_id("column_search_Description").clear()
+
+        # search description by column
+        self.browser.find_element_by_id("column_search_List Price").send_keys("3195")
+        table = self.browser.find_element_by_id('product_table')
+        self.assertIn(expected_table_content, table.text)
+        self.assertIn(r[1], table.text)
+        self.browser.find_element_by_id("column_search_List Price").clear()
