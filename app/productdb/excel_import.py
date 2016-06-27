ee2a58c39bc82ffe2fc4b696015868ba77faa32c
Henry Ölsner <henry@codingnetworker.com>
2016-06-27 22:36:54 +0200

    add optional parameters to the Excel import

--- app/productdb/excel_import.py
+++ app/productdb/excel_import.py
@@ -1,9 +1,10 @@
+import datetime
 import logging
 import pandas as pd
 from django.db import transaction
 from reversion import revisions as reversion
 from xlrd import XLRDError
-from app.productdb.models import Product
+from app.productdb.models import Product, CURRENCY_CHOICES
 from app.productdb.models import Vendor
 
 logger = logging.getLogger(__name__)
@@ -59,7 +60,14 @@ class ImportProductsExcelFile:
                 "currency": str,
                 "vendor": str
             }
-        ).dropna(axis=0, subset=["product id"])
+        )
+
+        # normalize the column names (all lowercase, strip whitespace if any)
+        self.__wb_data_frame__.columns = [x.lower() for x in self.__wb_data_frame__.columns]
+        self.__wb_data_frame__.columns = [x.strip() for x in self.__wb_data_frame__.columns]
+
+        # drop all values that are null in the product id column
+        self.__wb_data_frame__.dropna(axis=0, subset=["product id"], inplace=True)
 
     def verify_file(self):
         if self.workbook is None:
@@ -68,7 +76,7 @@ class ImportProductsExcelFile:
 
         sheets = self.workbook.sheet_names
         required_sheet = "products"
-        required_keys = {"product id", "description", "list price", "currency", "vendor"}
+        required_keys = {"product id", "description", "list price", "vendor"}
 
         # verify worksheet that is required
         if required_sheet not in sheets:
@@ -76,9 +84,9 @@ class ImportProductsExcelFile:
 
         # verify keys in file
         dframe = self.workbook.parse("products")
-        keys = set(dframe.keys())
+        keys = [x.lower() for x in set(dframe.keys())]
 
-        if len(required_keys.intersection(keys)) != 5:
+        if len(required_keys.intersection(keys)) != len(required_keys):
             raise InvalidImportFormatException("required keys not found in Excel file")
 
         self.valid_file = True
@@ -91,6 +99,33 @@ class ImportProductsExcelFile:
 
     def is_valid_file(self):
         return self.valid_file
+
+    def _import_datetime_column_from_file(self, row_key, row, target_key, product):
+        """
+        helper method to import an optional column from the excel file
+        """
+        changed = False
+        faulty_entry = False
+        msg = ""
+        try:
+            if row_key in row:
+                if not pd.isnull(row[row_key]):
+                    currval = getattr(product, target_key)
+                    if (type(row[row_key]) is pd.tslib.Timestamp) or (type(row[row_key]) is datetime.datetime):
+                        newval = row[row_key].date()
+
+                    else:
+                        newval = None
+
+                    if currval != newval:
+                        setattr(product, target_key, row[row_key].date())
+                        changed = True
+
+        except Exception as ex:
+            faulty_entry = True
+            msg = "cannot set %s for <code>%s</code> (%s)" % (row_key, row["product id"], ex)
+
+        return changed, faulty_entry, msg
 
     def import_products_to_database(self, status_callback=None):
         """
@@ -120,57 +155,132 @@ class ImportProductsExcelFile:
             changed = created
 
             # apply changes (only if a value is set, otherwise ignore it)
+            row_key = "description"
             try:
-                if not pd.isnull(row["description"]):
-                    if p.description != row["description"]:
-                        p.description = row["description"]
+                # set the description value
+                if not pd.isnull(row[row_key]):
+                    if p.description != row[row_key]:
+                        p.description = row[row_key]
                         changed = True
 
-            except Exception as ex:
-                faulty_entry = True
-                msg = "cannot set description for <code>%s</code> (%s)" % (row["product id"], ex)
+                # determine the list price and currency from the excel file
+                row_key = "list price"
+                new_currency = "USD"    # default in model
 
-            try:
-                if not pd.isnull(row["list price"]):
-                    if p.list_price != float(row["list price"]):
-                        p.list_price = row["list price"]
-                        changed = True
+                if not pd.isnull(row[row_key]):
+                    if type(row[row_key]) == float:
+                        new_price = row[row_key]
 
-            except Exception as ex:
-                faulty_entry = True
-                msg = "cannot set list price for <code>%s</code> (%s)" % (row["product id"], ex)
+                    elif type(row[row_key]) == str:
+                        price = row[row_key].split(" ")
+                        if len(price) == 1:
+                            new_price = float(row[row_key])
 
-            try:
-                if not pd.isnull(row["currency"]):
-                    if p.currency != row["currency"]:
-                        p.currency = row["currency"]
-                        changed = True
+                        elif len(price) == 2:
+                            try:
+                                new_price = float(price[0])
+                            except:
+                                raise Exception("cannot convert price information to float")
 
-            except Exception as ex:
-                faulty_entry = True
-                msg = "cannot set currency for <code>%s</code> (%s)" % (row["product id"], ex)
+                            # check valid currency value
+                            valid_currency = True if price[1].upper() in dict(CURRENCY_CHOICES).keys() else False
+                            if valid_currency:
+                                new_currency = price[1].upper()
 
-            try:
+                            else:
+                                raise Exception("cannot set currency unknown value %s" % price[1].upper())
+
+                        else:
+                            raise Exception("invalid format for list price, detected multiple spaces")
+
+                    else:
+                        logger.debug("list price data type for %s identified as %s" % (
+                            row["product id"],
+                            str(type(row[row_key]))
+                        ))
+                        raise Exception("invalid data-type for list price")
+                else:
+                    new_price = None
+
+                row_key = "currency"
+                if row_key in row:
+                    if not pd.isnull(row[row_key]):
+                        # check valid currency value
+                        valid_currency = True if row[row_key].upper() in dict(CURRENCY_CHOICES).keys() else False
+                        if valid_currency:
+                            new_currency = row[row_key].upper()
+
+                        else:
+                            raise Exception("cannot set currency unknown value %s" % row[row_key].upper())
+
+                # apply the new list price and currency if required
+                if p.list_price != new_price:
+                    p.list_price = new_price
+                    changed = True
+                if p.currency != new_currency:
+                    p.currency = new_currency
+                    changed = True
+
                 # set vendor to unassigned (ID 0) if no Vendor is provided and the product was created
-                if pd.isnull(row["vendor"]) and created:
+                row_key = "vendor"
+                if pd.isnull(row[row_key]) and created:
                     v = Vendor.objects.get(id=0)
                     changed = True
                     p.vendor = v
 
-                elif not pd.isnull(row["vendor"]):
-                    if p.vendor.name != row["vendor"]:
+                elif not pd.isnull(row[row_key]):
+                    if p.vendor.name != row[row_key]:
                         try:
-                            v = Vendor.objects.get(name=row["vendor"])
+                            v = Vendor.objects.get(name=row[row_key])
 
                         except Vendor.DoesNotExist:
-                            raise Exception("Vendor <strong>%s</strong> doesn't exist" % row["vendor"])
+                            raise Exception("Vendor <strong>%s</strong> doesn't exist" % row[row_key])
 
                         changed = True
                         p.vendor = v
 
+                # set Eol note URL and friendly name (both optional)
+                row_key = "eol note url"
+                if row_key in row:
+                    if not pd.isnull(row[row_key]):
+                        if p.eol_reference_url != row[row_key]:
+                            p.eol_reference_url = row[row_key]
+                            changed = True
+
+                row_key = "eol note url (friendly name)"
+                if row_key in row:
+                    if not pd.isnull(row[row_key]):
+                        if p.eol_reference_number != row[row_key]:
+                            p.eol_reference_number = row[row_key]
+
             except Exception as ex:
                 faulty_entry = True
-                msg = "cannot set vendor for <code>%s</code> (%s)" % (row["product id"], ex)
+                msg = "cannot set %s for <code>%s</code> (%s)" % (row_key, row["product id"], ex)
+
+            # import datetime columns from file
+            data_map = {
+                # product attribute - data frame column name (lowered during the import)
+                "eox_update_time_stamp": "eox update timestamp",
+                "eol_ext_announcement_date": "eol announcement date",
+                "end_of_sale_date": "end of sale date",
+                "end_of_new_service_attachment_date": "end of new service attachment date",
+                "end_of_sw_maintenance_date": "end of sw maintenance date",
+                "end_of_routine_failure_analysis": "end of routing failure analysis date",
+                "end_of_service_contract_renewal": "end of service contract renewal date",
+                "end_of_support_date": "last date of support",
+                "end_of_sec_vuln_supp_date": "end of security/vulnerability support date"
+            }
+
+            for key in data_map.keys():
+                c, f, ret_msg = self._import_datetime_column_from_file(data_map[key], row, key, p)
+                if c:
+                    # value was changed
+                    changed = True
+                if f:
+                    # value was faulty
+                    msg = ret_msg
+                    faulty_entry = True
+                    break
 
             # save result to database if any
             try:
@@ -201,10 +311,10 @@ class ImportProductsExcelFile:
 
             except Exception as ex:
                 faulty_entry = True
-                msg = "cannot save data for <code>%s</code> (%s)" % (row["product id"], ex)
+                msg = "cannot save data for <code>%s</code> in database (%s)" % (row["product id"], ex)
 
             if faulty_entry:
-                logger.error(msg)
+                logger.error("cannot import %s (%s)" % (row["product id"], msg))
                 self.import_result_messages.append(msg)
                 self.invalid_products += 1
 
--- app/productdb/tests/test_excel_import.py
+++ app/productdb/tests/test_excel_import.py
@@ -5,12 +5,35 @@ from django.test import TestCase
 import os
 from app.productdb.excel_import import ImportProductsExcelFile, InvalidExcelFileFormat, InvalidImportFormatException
 from app.productdb.models import Product
+import datetime
 
 
 class TestBaseExcelImport(TestCase):
     fixtures = ['default_vendors.yaml']
 
-    def test_valid_product_import_using_excel_file(self):
+    @staticmethod
+    def prepare_import_products_excel_file(filename, verify_file=True, start_import=True):
+        """
+        helping method, that creates a new ImportProductsExcelFile instance based on a "filename" in the test data
+        directory
+        """
+        valid_test_file = os.path.join(os.getcwd(),
+                                       "tests",
+                                       "data",
+                                       filename)
+
+        product_file = ImportProductsExcelFile(valid_test_file)
+        if verify_file:
+            product_file.verify_file()
+        if start_import:
+            product_file.import_products_to_database()
+
+        return product_file
+
+    def test_valid_product_import_using_excel_with_currency_column(self):
+        """
+        test a valid product import using the standard Excel template with separate list price and currency columns
+        """
         test_product_ids = [
             'WS-C2960S-48FPD-L',
             'WS-C2960S-48LPD-L',
@@ -87,9 +110,285 @@ class TestBaseExcelImport(TestCase):
         for product in products:
             p = Product.objects.get(product_id=product['product id'])
             self.assertEqual(p.description, product['description'])
+            self.assertEqual(p.list_price, product['list price'], p.product_id)
+            self.assertEqual(p.currency, product['currency'])
+            self.assertEqual(p.vendor.name, product['vendor'])
+
+    def test_valid_product_import_using_excel_without_currency_column(self):
+        """
+        test a valid product import using the standard Excel template with list price
+        """
+        test_product_ids = [
+            'WS-C2960S-48FPD-L',
+            'WS-C2960S-48LPD-L',
+            'WS-C2960S-24PD-L',
+            'WS-C2960S-48TD-L',
+            'WS-C2960S-24TD-L',
+            'WS-C2960S-48FPS-L',
+            'WS-C2960S-48LPS-L',
+            'WS-C2960S-24PS-L',
+            'CAB-STK-E-0.5M',
+            'CAB-STK-E-1M=',
+            'CAB-STK-E-1M',
+            'CAB-STK-E-3M=',
+            'CAB-CONSOLE-RJ45=',
+            'CAB-CONSOLE-USB=',
+            'EX4200-24F',
+            'EX4200-24F-DC',
+            'WS-C2960S-48TS-L',
+            'WS-C2960S-24TS-L',
+            'WS-C2960S-48TS-S',
+            'WS-C2960S-24TS-S',
+            'C2960S-STACK',
+            'C2960S-STACK=',
+            'CAB-STK-E-0.5M=',
+            'EX4200-24F-S',
+            'EX4200-24PX',
+        ]
+        products = [
+            {
+                'product id': 'WS-C2960S-48FPD-L',
+                'description': 'Catalyst 2960S 48 GigE PoE 740W, 2 x 10G SFP+ LAN Base',
+                'list price': 8795,
+                'currency': 'USD',
+                'vendor': 'Cisco Systems',
+            },
+            {
+                'product id': 'CAB-STK-E-1M',
+                'description': 'Cisco FlexStack 1m stacking cable',
+                'list price': 100,
+                'currency': 'USD',
+                'vendor': 'unassigned',
+            },
+            {
+                'product id': 'CAB-STK-E-1M=',
+                'description': 'Cisco Bladeswitch 1M stack cable',
+                'list price': None,
+                'currency': 'USD',
+                'vendor': 'Cisco Systems',
+            },
+
+        ]
+        valid_test_file = os.path.join(os.getcwd(),
+                                       "tests",
+                                       "data",
+                                       "excel_import_products_test-wo_currency.xlsx")
+
+        product_file = ImportProductsExcelFile(valid_test_file)
+        product_file.verify_file()
+
+        self.assertTrue(product_file.valid_file, "given excel file has no valid format")
+
+        product_file.import_products_to_database()
+
+        self.assertEqual(product_file.valid_imported_products, 25, "\n".join([l for l in product_file.import_result_messages]))
+        self.assertEqual(product_file.invalid_products, 0)
+        self.assertEqual(product_file.amount_of_products, 25)
+        self.assertIsNotNone(product_file.import_result_messages)
+
+        # verify that the expected products are created in the database
+        for pid in test_product_ids:
+            Product.objects.get(product_id=pid)
+
+        # look at the imported values from the
+        for product in products:
+            p = Product.objects.get(product_id=product['product id'])
+            self.assertEqual(p.description, product['description'])
             self.assertEqual(p.list_price, product['list price'])
             self.assertEqual(p.currency, product['currency'])
             self.assertEqual(p.vendor.name, product['vendor'])
+
+    def test_valid_product_import_with_eox_update_timestamp_date_using_excel_file(self):
+        lc_product_id = "WS-C2960S-48FPD-L"
+        no_lc_product_id = "EX4200-24PX"
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+        no_lc_product = Product.objects.get(product_id=no_lc_product_id)
+
+        self.assertIsNotNone(lc_product.eox_update_time_stamp)
+        self.assertIsNone(no_lc_product.eox_update_time_stamp)
+        self.assertEqual(datetime.date(2016, 1, 1), lc_product.eox_update_time_stamp)
+
+    def test_valid_product_import_with_eol_announcement_date_using_excel_file(self):
+        lc_product_id = "WS-C2960S-48LPD-L"
+        no_lc_product_id = "EX4200-24PX"
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+        no_lc_product = Product.objects.get(product_id=no_lc_product_id)
+
+        self.assertIsNotNone(lc_product.eol_ext_announcement_date)
+        self.assertIsNone(no_lc_product.eol_ext_announcement_date)
+        self.assertEqual(datetime.date(2016, 1, 1), lc_product.eol_ext_announcement_date)
+
+    def test_valid_product_import_with_eos_date_using_excel_file(self):
+        lc_product_id = "WS-C2960S-24PD-L"
+        no_lc_product_id = "EX4200-24PX"
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+        no_lc_product = Product.objects.get(product_id=no_lc_product_id)
+
+        self.assertIsNotNone(lc_product.end_of_sale_date)
+        self.assertIsNone(no_lc_product.end_of_sale_date)
+        self.assertEqual(datetime.date(2016, 1, 1), lc_product.end_of_sale_date)
+
+    def test_valid_product_import_with_end_of_new_service_attachment_date_using_excel_file(self):
+        lc_product_id = "WS-C2960S-48TD-L"
+        no_lc_product_id = "EX4200-24PX"
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+        no_lc_product = Product.objects.get(product_id=no_lc_product_id)
+
+        self.assertIsNone(lc_product.end_of_sale_date)
+        self.assertIsNotNone(lc_product.end_of_new_service_attachment_date)
+        self.assertIsNone(no_lc_product.end_of_new_service_attachment_date)
+        self.assertEqual(datetime.date(2016, 1, 1), lc_product.end_of_new_service_attachment_date)
+
+    def test_valid_product_import_with_end_of_sw_maintenance_date_using_excel_file(self):
+        lc_product_id = "WS-C2960S-24TD-L"
+        no_lc_product_id = "EX4200-24PX"
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+        no_lc_product = Product.objects.get(product_id=no_lc_product_id)
+
+        self.assertIsNone(lc_product.end_of_sale_date)
+        self.assertIsNotNone(lc_product.end_of_sw_maintenance_date)
+        self.assertIsNone(no_lc_product.end_of_sw_maintenance_date)
+        self.assertEqual(datetime.date(2016, 1, 1), lc_product.end_of_sw_maintenance_date)
+
+    def test_valid_product_import_with_end_of_routine_failure_analysis_using_excel_file(self):
+        lc_product_id = "WS-C2960S-48FPS-L"
+        no_lc_product_id = "EX4200-24PX"
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+        no_lc_product = Product.objects.get(product_id=no_lc_product_id)
+
+        self.assertIsNone(lc_product.end_of_sale_date)
+        self.assertIsNotNone(lc_product.end_of_routine_failure_analysis)
+        self.assertIsNone(no_lc_product.end_of_routine_failure_analysis)
+        self.assertEqual(datetime.date(2016, 1, 1), lc_product.end_of_routine_failure_analysis)
+
+    def test_valid_product_import_with_end_of_service_contract_renewal_using_excel_file(self):
+        lc_product_id = "WS-C2960S-48LPS-L"
+        no_lc_product_id = "EX4200-24PX"
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+        no_lc_product = Product.objects.get(product_id=no_lc_product_id)
+
+        self.assertIsNone(lc_product.end_of_sale_date)
+        self.assertIsNotNone(lc_product.end_of_service_contract_renewal)
+        self.assertIsNone(no_lc_product.end_of_service_contract_renewal)
+        self.assertEqual(datetime.date(2016, 1, 1), lc_product.end_of_service_contract_renewal)
+
+    def test_valid_product_import_with_end_of_support_date_using_excel_file(self):
+        lc_product_id = "WS-C2960S-24PS-L"
+        no_lc_product_id = "EX4200-24PX"
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+        no_lc_product = Product.objects.get(product_id=no_lc_product_id)
+
+        self.assertIsNone(lc_product.end_of_sale_date)
+        self.assertIsNotNone(lc_product.end_of_support_date)
+        self.assertIsNone(no_lc_product.end_of_support_date)
+        self.assertEqual(datetime.date(2016, 1, 1), lc_product.end_of_support_date)
+
+    def test_valid_product_import_with_end_of_sec_vuln_supp_date_using_excel_file(self):
+        lc_product_id = "CAB-STK-E-0.5M"
+        no_lc_product_id = "EX4200-24PX"
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+        no_lc_product = Product.objects.get(product_id=no_lc_product_id)
+
+        self.assertIsNone(lc_product.end_of_sale_date)
+        self.assertIsNotNone(lc_product.end_of_sec_vuln_supp_date)
+        self.assertIsNone(no_lc_product.end_of_sec_vuln_supp_date)
+        self.assertEqual(datetime.date(2016, 1, 1), lc_product.end_of_sec_vuln_supp_date)
+
+    def test_valid_product_import_with_eol_note_using_excel_file(self):
+        lc_product_id = "CAB-STK-E-1M="
+        no_lc_product_id = "EX4200-24PX"
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+        no_lc_product = Product.objects.get(product_id=no_lc_product_id)
+
+        self.assertIsNone(lc_product.end_of_sale_date)
+        self.assertIsNotNone(lc_product.eol_reference_url)
+        self.assertIsNotNone(lc_product.eol_reference_number)
+        self.assertIsNone(no_lc_product.eol_reference_url)
+        self.assertEqual("http://localhost/myurl", lc_product.eol_reference_url)
+        self.assertEqual("My Friendly Name", lc_product.eol_reference_number)
+
+        # test without eol_reference_number
+        lc_product_id = "CAB-STK-E-1M"
+        lc_product = Product.objects.get(product_id=lc_product_id)
+
+        self.assertIsNotNone(lc_product.eol_reference_url)
+        self.assertIsNone(lc_product.eol_reference_number)
+        self.assertEqual("http://localhost/myurl", lc_product.eol_reference_url)
+
+    def test_valid_product_import_with_full_lifecycle_data_using_excel_file(self):
+        lc_product_id = "CAB-STK-E-3M="
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+
+        self.assertEqual(datetime.date(2016, 1, 1), lc_product.eox_update_time_stamp)
+        self.assertEqual(datetime.date(2016, 1, 2), lc_product.eol_ext_announcement_date)
+        self.assertEqual(datetime.date(2016, 1, 3), lc_product.end_of_sale_date)
+        self.assertEqual(datetime.date(2016, 1, 4), lc_product.end_of_new_service_attachment_date)
+        self.assertEqual(datetime.date(2016, 1, 5), lc_product.end_of_sw_maintenance_date)
+        self.assertEqual(datetime.date(2016, 1, 6), lc_product.end_of_routine_failure_analysis)
+        self.assertEqual(datetime.date(2016, 1, 7), lc_product.end_of_service_contract_renewal)
+        self.assertEqual(datetime.date(2016, 1, 8), lc_product.end_of_sec_vuln_supp_date)
+        self.assertEqual(datetime.date(2016, 1, 9), lc_product.end_of_support_date)
+        self.assertEqual("http://localhost", lc_product.eol_reference_url)
+        self.assertEqual("comment", lc_product.eol_reference_number)
+
+    def test_change_product_list_price_by_excel_upload_with_separate_currency_column(self):
+        lc_product_id = "WS-C2960S-48FPD-L"
+        lc_product = Product.objects.create(product_id=lc_product_id)
+        lc_product.list_price = 1.0
+        lc_product.save()
+
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-with_eol_data.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+
+        self.assertEqual(8795.0, lc_product.list_price)
+
+    def test_change_product_list_price_by_excel_upload_without_separate_currency_column(self):
+        lc_product_id = "WS-C2960S-48FPD-L"
+        lc_product = Product.objects.create(product_id=lc_product_id)
+        lc_product.list_price = 1.0
+        lc_product.save()
+
+        _ = self.prepare_import_products_excel_file("excel_import_products_test-wo_currency.xlsx")
+
+        # verify the lifecycle information for the test products
+        lc_product = Product.objects.get(product_id=lc_product_id)
+
+        self.assertEqual(8795.0, lc_product.list_price)
 
     def test_invalid_product_import_using_excel_file_with_invalid_keys(self):
         valid_test_file = os.path.join(os.getcwd(),
