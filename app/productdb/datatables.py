d9600350b2b88d49e86e8dc87a2cd77fa3a474a4
Henry Ölsner <henry@codingnetworker.com>
2016-07-12 23:07:22 +0200

    refactor datatables class implementation

--- app/productdb/datatables.py
+++ app/productdb/datatables.py
@@ -4,11 +4,33 @@ from django.db.models import Q
 from app.productdb.utils import is_valid_regex
 
 
-class VendorProductListJson(BaseDatatableView):
-    # define column names that will be used in sorting
-    # order is important and should be same as order of columns
-    # displayed by datatables. For non sortable columns use empty
-    # value like ''
+class ColumnSearchMixin:
+    """
+    column search implementation for datatables
+    """
+    # the following dictionary is required in the class, that implements the column based search function
+    column_based_filter = {}
+
+    def apply_column_based_search(self, request, query_set):
+        """
+        apply the column based search parameters from datatables to the query_set
+        """
+        # apply column based search parameters
+        for name, param in self.column_based_filter.items():
+            get_param = 'columns[' + str(param["order"]) + '][search][value]'
+            column_search_string = request.GET.get(get_param, None)
+
+            if column_search_string:
+                query_set = query_set.filter(**{
+                    "%s__%s" % (
+                        param["expr"],
+                        "regex" if is_valid_regex(column_search_string) else "contains"
+                    ): column_search_string
+                })
+        return query_set
+
+
+class VendorProductListJson(BaseDatatableView, ColumnSearchMixin):
     order_columns = [
         'product_id',
         'product_group',
@@ -16,12 +38,30 @@ class VendorProductListJson(BaseDatatableView):
         'list_price',
         'tags'
     ]
-    column_order = {
-        "product_id": 0,
-        "product_group": 1,
-        "description": 2,
-        "list_price": 3,
-        "tags": 4
+
+    column_based_filter = {  # parameters that are required for the column based filtering
+        "product_id": {
+            "order": 0,
+            "expr": "product_id",
+        },
+        "product_group": {
+            "order": 1,
+            "expr": "product_group__name",
+        },
+        "description": {
+            "order": 2,
+            "expr": "description",
+        }
+        ,
+        "list_price": {
+            "order": 3,
+            "expr": "list_price",
+        },
+        "tags": {
+            "order": 4,
+            "expr": "tags",
+        }
+
     }
 
     vendor_id = 0
@@ -29,72 +69,25 @@ class VendorProductListJson(BaseDatatableView):
     def get_initial_queryset(self):
         if self.kwargs['vendor_id']:
             self.vendor_id = self.kwargs['vendor_id']
-
-        # return queryset used as base for further sorting/filtering
-        # these are simply objects displayed in datatable
         return Product.objects.filter(vendor__id=self.vendor_id)
 
     def filter_queryset(self, qs):
-        # use request parameters to filter queryset
         search_string = self.request.GET.get('search[value]', None)
 
-        product_id_get_param = 'columns[' + str(self.column_order["product_id"]) + '][search][value]'
-        product_group_get_param = 'columns[' + str(self.column_order["product_group"]) + '][search][value]'
-        description_get_param = 'columns[' + str(self.column_order["description"]) + '][search][value]'
-        list_price_get_param = 'columns[' + str(self.column_order["list_price"]) + '][search][value]'
-        tags_get_param = 'columns[' + str(self.column_order["tags"]) + '][search][value]'
-
-        product_id_search = self.request.GET.get(product_id_get_param, None)
-        product_group_search = self.request.GET.get(product_group_get_param, None)
-        description_search = self.request.GET.get(description_get_param, None)
-        list_price_search = self.request.GET.get(list_price_get_param, None)
-        tags_search = self.request.GET.get(tags_get_param, None)
-
         if search_string:
-            # by default, the search field searched in the Product ID and the description field
-            if is_valid_regex(search_string):
-                qs = qs.filter(Q(product_id__regex=search_string) |
-                               Q(description__regex=search_string))
-            else:
-                qs = qs.filter(Q(product_id__contains=search_string) |
-                               Q(description__contains=search_string))
+            # search in the Product Group name and Vendor name by default
+            operation = "regex" if is_valid_regex(search_string) else "contains"
+            qs = qs.filter(
+                Q(**{"product_id__%s" % operation: search_string}) |
+                Q(**{"description__%s" % operation: search_string})
+            )
 
-        if product_id_search:
-            if is_valid_regex(product_id_search):
-                qs = qs.filter(product_id__regex=product_id_search)
-            else:
-                qs = qs.filter(product_id__contains=product_id_search)
-
-        if product_group_search:
-            # the property for the product group is hidden in the class, therefore a "_" as prefix is required
-            if is_valid_regex(product_group_search):
-                qs = qs.filter(product_group__name__regex=product_group_search)
-            else:
-                qs = qs.filter(product_group__name__contains=product_group_search)
-
-        if description_search:
-            if is_valid_regex(description_search):
-                qs = qs.filter(description__regex=description_search)
-            else:
-                qs = qs.filter(description__contains=description_search)
-
-        if list_price_search:
-            if is_valid_regex(list_price_search):
-                qs = qs.filter(list_price__regex=list_price_search)
-            else:
-                qs = qs.filter(list_price__contains=list_price_search)
-
-        if tags_search:
-            if is_valid_regex(tags_search):
-                qs = qs.filter(tags__regex=tags_search)
-            else:
-                qs = qs.filter(tags__contains=tags_search)
+        # apply column based search
+        qs = self.apply_column_based_search(request=self.request, query_set=qs)
 
         return qs
 
     def prepare_results(self, qs):
-        # prepare list with output column data
-        # queryset is already paginated here
         json_data = []
 
         for item in qs:
@@ -123,7 +116,7 @@ class VendorProductListJson(BaseDatatableView):
         return json_data
 
 
-class ListProductGroupsJson(BaseDatatableView):
+class ListProductGroupsJson(BaseDatatableView, ColumnSearchMixin):
     """
     Product Group datatable endpoint
     """
@@ -157,18 +150,8 @@ class ListProductGroupsJson(BaseDatatableView):
                 Q(**{"vendor__name__%s" % operation: search_string})
             )
 
-        # apply column based search parameters
-        for name, param in self.column_based_filter.items():
-            get_param = 'columns[' + str(param["order"]) + '][search][value]'
-            column_search_string = self.request.GET.get(get_param, None)
-
-            if column_search_string:
-                qs = qs.filter(**{
-                    "%s__%s" % (
-                        param["expr"],
-                        "regex" if is_valid_regex(column_search_string) else "contains"
-                    ): column_search_string
-                })
+        # apply column based search
+        qs = self.apply_column_based_search(request=self.request, query_set=qs)
 
         return qs
 
@@ -185,7 +168,7 @@ class ListProductGroupsJson(BaseDatatableView):
         return json_data
 
 
-class ListProductsByGroupJson(BaseDatatableView):
+class ListProductsByGroupJson(BaseDatatableView, ColumnSearchMixin):
     """
     Product datatables endpoint for a a specific Product Group
     """
@@ -237,24 +220,12 @@ class ListProductsByGroupJson(BaseDatatableView):
                 Q(**{"description__%s" % operation: search_string})
             )
 
-        # apply column based search parameters
-        for name, param in self.column_based_filter.items():
-            get_param = 'columns[' + str(param["order"]) + '][search][value]'
-            column_search_string = self.request.GET.get(get_param, None)
-
-            if column_search_string:
-                qs = qs.filter(**{
-                    "%s__%s" % (
-                        param["expr"],
-                        "regex" if is_valid_regex(column_search_string) else "contains"
-                    ): column_search_string
-                })
+        # apply column based search
+        qs = self.apply_column_based_search(request=self.request, query_set=qs)
 
         return qs
 
     def prepare_results(self, qs):
-        # prepare list with output column data
-        # queryset is already paginated here
         json_data = []
 
         for item in qs:
@@ -281,11 +252,7 @@ class ListProductsByGroupJson(BaseDatatableView):
         return json_data
 
 
-class ListProductsJson(BaseDatatableView):
-    # define column names that will be used in sorting
-    # order is important and should be same as order of columns
-    # displayed by datatables. For non sortable columns use empty
-    # value like ''
+class ListProductsJson(BaseDatatableView, ColumnSearchMixin):
     order_columns = [
         'vendor',
         'product_id',
@@ -294,89 +261,54 @@ class ListProductsJson(BaseDatatableView):
         'list_price',
         'tags'
     ]
-    column_order = {
-        "vendor": 0,
-        "product_id": 1,
-        "product_group": 2,
-        "description": 3,
-        "list_price": 4,
-        "tags": 5
+    column_based_filter = {  # parameters that are required for the column based filtering
+        "vendor": {
+            "order": 0,
+            "expr": "vendor__name"
+        },
+        "product_id": {
+            "order": 1,
+            "expr": "product_id"
+        },
+        "product_group": {
+            "order": 2,
+            "expr": "product_group__name"
+        },
+        "description": {
+            "order": 3,
+            "expr": "description"
+        },
+        "list_price": {
+            "order": 4,
+            "expr": "list_price"
+        },
+        "tags": {
+            "order": 5,
+            "expr": "tags"
+        }
     }
 
     def get_initial_queryset(self):
-        # return queryset used as base for further sorting/filtering
-        # these are simply objects displayed in datatable
         return Product.objects.all()
 
     def filter_queryset(self, qs):
         # use request parameters to filter queryset
         search_string = self.request.GET.get('search[value]', None)
 
-        vendor_get_param = 'columns[' + str(self.column_order["vendor"]) + '][search][value]'
-        product_id_get_param = 'columns[' + str(self.column_order["product_id"]) + '][search][value]'
-        product_group_get_param = 'columns[' + str(self.column_order["product_group"]) + '][search][value]'
-        description_get_param = 'columns[' + str(self.column_order["description"]) + '][search][value]'
-        list_price_get_param = 'columns[' + str(self.column_order["list_price"]) + '][search][value]'
-        tags_get_param = 'columns[' + str(self.column_order["tags"]) + '][search][value]'
-
-        vendor_search = self.request.GET.get(vendor_get_param, None)
-        product_id_search = self.request.GET.get(product_id_get_param, None)
-        product_group_search = self.request.GET.get(product_group_get_param, None)
-        description_search = self.request.GET.get(description_get_param, None)
-        list_price_search = self.request.GET.get(list_price_get_param, None)
-        tags_search = self.request.GET.get(tags_get_param, None)
-
         if search_string:
-            # by default, the search field searched in the Product ID and the description field
-            if is_valid_regex(search_string):
-                qs = qs.filter(Q(product_id__regex=search_string) |
-                               Q(description__regex=search_string))
-            else:
-                qs = qs.filter(Q(product_id__contains=search_string) |
-                               Q(description__contains=search_string))
+            # search in the Product Group name and Vendor name by default
+            operation = "regex" if is_valid_regex(search_string) else "contains"
+            qs = qs.filter(
+                Q(**{"product_id__%s" % operation: search_string}) |
+                Q(**{"description__%s" % operation: search_string})
+            )
 
-        if vendor_search:
-            if is_valid_regex(vendor_search):
-                qs = qs.filter(vendor__name__regex=vendor_search)
-            else:
-                qs = qs.filter(vendor__name__contains=vendor_search)
-
-        if product_id_search:
-            if is_valid_regex(product_id_search):
-                qs = qs.filter(product_id__regex=product_id_search)
-            else:
-                qs = qs.filter(product_id__contains=product_id_search)
-
-        if product_group_search:
-            # the property for the product group is hidden in the class, therefore a "_" as prefix is required
-            if is_valid_regex(product_group_search):
-                qs = qs.filter(product_group__name__regex=product_group_search)
-            else:
-                qs = qs.filter(product_group__name__contains=product_group_search)
-
-        if description_search:
-            if is_valid_regex(description_search):
-                qs = qs.filter(description__regex=description_search)
-            else:
-                qs = qs.filter(description__contains=description_search)
-
-        if list_price_search:
-            if is_valid_regex(list_price_search):
-                qs = qs.filter(list_price__regex=list_price_search)
-            else:
-                qs = qs.filter(list_price__contains=list_price_search)
-
-        if tags_search:
-            if is_valid_regex(tags_search):
-                qs = qs.filter(tags__regex=tags_search)
-            else:
-                qs = qs.filter(tags__contains=tags_search)
+        # apply column based search
+        qs = self.apply_column_based_search(request=self.request, query_set=qs)
 
         return qs
 
     def prepare_results(self, qs):
-        # prepare list with output column data
-        # queryset is already paginated here
         json_data = []
 
         for item in qs:
