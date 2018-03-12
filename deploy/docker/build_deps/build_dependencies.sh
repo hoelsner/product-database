1cbcd3fd0c1c4ba1969996d584548a57e5cb91b8
Henry Ölsner <henry@codingnetworker.com>
2018-03-12 21:01:45 +0100

    add docker compose file for the test instance

--- deploy/docker/build_deps/build_dependencies.sh
+++ deploy/docker/build_deps/build_dependencies.sh
@@ -22,6 +22,14 @@ then
     python3 manage.py createcachetable
     python3 manage.py loaddata default_vendors default_text_blocks default_users
     touch $flag_file
+
+    if [ "$LOAD_SELENIUM_TEST_DATA" == "1" ]
+    then
+        echo ""
+        echo "==> load test data for the selenium test cases"
+        echo ""
+        python3 manage.py loaddata selenium_tests
+    fi
 else
     echo ""
     echo "==> don't create database defaults"
@@ -32,7 +40,7 @@ file="/var/www/productdb/ssl/server.key"
 if [ ! -f "$file" ]
 then
     echo ""
-    echo "==> SSL certificate not found, generate HTTPs certificate..."
+    echo "==> SSL certificate not found, generate self-signed HTTPs certificate..."
     echo ""
     openssl req -new -nodes -x509 -subj "/C=$HTTPS_SELF_SIGNED_CERT_COUNTRY/CN=$HTTPS_SELF_SIGNED_CERT_FQDN" -days 3650 -keyout /var/www/productdb/ssl/server.key -out /var/www/productdb/ssl/server.crt -extensions v3_ca
 else
